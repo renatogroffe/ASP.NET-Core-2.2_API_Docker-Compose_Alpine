123492083e0626dc87119b143e4c679ed592a2ac
Renato Groffe <renato.groffe@yahoo.com.br>
2018-12-26 23:51:48 -0200

    ASP.NET Core 2.2 + .NET Core 2.2 + Docker + Alpine Linux + Docker Compose
    ASP.NET Core 2.2 + .NET Core 2.2 + Docker + Alpine Linux + Docker Compose

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIContagem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get()
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString
+                };
+            }
+        }
+    }
+}
